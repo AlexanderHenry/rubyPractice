e6bba090b8cbf74dbb05ec9eb1f525d06d11645e
AlexanderHenry <alexanderhenry21@gmail.com>
2016-04-20 19:16:34 -0500

    Cleaned up pizza party code.  No negatives, zeroes, or blank values.

--- pizzaParty.rb
+++ pizzaParty.rb
@@ -1,11 +1,43 @@
-def party
-
-	puts "How many people?"
-	people = gets.to_i
-	puts "How many pizzas do you have?"
-	pizzas = gets.to_i
+def prompt_for_slices
 	puts "How many slices per pizza?"
 	slices = gets.to_i
+end
+
+def prompt_for_pizzas
+	puts "How many pizzas do you have?"
+	pizzas = gets.to_i
+end
+
+def prompt_for_people
+	puts "How many people?"
+	people = gets.to_i
+end
+
+def party
+	people = prompt_for_people
+	
+	while people <= 0
+		puts "Negative values don't make sense!"
+		people = prompt_for_people
+	end
+
+	pizzas = prompt_for_pizzas
+
+	while pizzas <= 0
+		puts "If you don't have any pizzas, then why are you using this app?"
+		pizzas = prompt_for_pizzas
+	end
+
+	slices = prompt_for_slices
+
+	while slices < 2
+
+		puts "There can't be negative slices!" if slices < 0
+		puts "That's the entire pie!" if slices == 0
+		puts "Is half of the pizza even a slice?" if slices == 1
+
+		slices = prompt_for_slices
+	end
 
 	totalSlices = slices * pizzas
 	slicesPerPerson = totalSlices / people
